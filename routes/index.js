405fb4b016e2bc29be73f93c4dd6516b01810553
VictorAbraham <victorabraham1988@gmail.com>
2020-02-28 01:01:29 -0700

    route update

--- routes/index.js
+++ routes/index.js
@@ -14,9 +14,7 @@ module.exports = (app) => {
 
   app.post('/api/todos/:todoId/items', todoItemsController.create);
   app.put('/api/todos/:todoId/items/:todoItemId', todoItemsController.update);
-  app.delete(
-    '/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy
-  );
+  app.delete('/api/todos/:todoId/items/:todoItemId', todoItemsController.destroy);
 
   // For any other request method on todo items, we're going to return "Method Not Allowed"
   app.all('/api/todos/:todoId/items', (req, res) =>
